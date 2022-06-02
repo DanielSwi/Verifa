7494b9ea14b2417a963cf9573fb6418389c0f696
DanielSwi <92669947+DanielSwi@users.noreply.github.com>
2022-06-02 17:36:45 +0200

    Update settings.py

--- first_web/first_web/settings.py
+++ first_web/first_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qfsphmz8ey^9@k_o9xiq5)+f-$4a*)+cggatd_bhhe6u^c)gjt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
